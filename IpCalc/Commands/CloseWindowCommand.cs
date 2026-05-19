17a20dc328ce88053a2583f6db96f3bef99b1a5a
Dmitriy Morozov <mitriy.exe@gmail.com>
2026-05-19 18:00:03 +0400

    Продолжаем пилить кнопки TODO: починить namespase

---
+++ IpCalc/Commands/CloseWindowCommand.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace IpCalc.Commands
+{
+    public class CloseWindowCommand : TriggerAction<DependencyObject>
+    {
+        protected override void Invoke(object parameter)
+        {
+            var window = Window.GetWindow(AssociatedObject);
+            window.Close();
+        }
+    }
+}
---
+++ IpCalc/Commands/MinimizeWindowCommand.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace IpCalc.Commands
+{
+    internal class MinimizeWindowCommand : TriggerAction<DependencyObject>
+    {
+        protected override void Invoke(object parameter)
+        {
+            var window = Window.GetWindow(AssociatedObject);
+            if (window != null)
+            {
+                window.WindowState = WindowState.Minimized;
+            }
+        }
+    }
+}
